d3f033b631cebe8533d5a460dfb6e7f342aeef53
Vinay2334 <98982309+Vinay2334@users.noreply.github.com>
2022-10-09 19:43:24 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI=('mongodb+srv://Vinay:9968393810ab@cluster0.blhljtt.mongodb.net/bankingapp?retryWrites=true&w=majority');
+const mongoURI=('');
 
 const connectToMongo=()=>{
     mongoose.connect(mongoURI,()=>{
